ca28e64f6e4858b825b07286edf640dd4e2cb6df
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-07-25 20:30:21 -0400

    failed 2 test :(

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders text MickeyMouse', () => {
+  render(<App />)
+  const nameElement = screen.getByText(/mickey mouse/i)
+  expect(nameElement).toBeInTheDocument()
+});
